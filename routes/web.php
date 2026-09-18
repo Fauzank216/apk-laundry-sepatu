b7d67d2e44daa252f25f4b969a53f24e070effd7
Fauzan <xyzan3585@gmail.com>
2026-09-19 05:47:45 +0700

    menambahkan route transaksi dan riwayat transaksi

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,23 @@ Route::get('/', function () {
 Route::get('/admin/dashboard', function () {
     return view('admin.index');
 });
+
+Route::get('/admin/petugas', function () {
+    return view('admin.petugas.index');
+});
+
+Route::get('/admin/pelanggan', function () {
+    return view('admin.pelanggan.index');
+});
+
+Route::get('/admin/layanan', function () {
+    return view('admin.layanan.index');
+});
+
+Route::get('/transaksi', function () {
+    return view('admin.transaksi.index');
+});
+
+Route::get('/transaksi/history', function () {
+    return view('admin.transaksi.history');
+});
